e5d22b6eb1082500bbf51e9a137366866ee35cc0
Gazi Salahuddin <71849295+Salahuddin-Gazi@users.noreply.github.com>
2022-01-07 23:45:55 +0600

    Fixed Paths

--- src/components/routing/AppRoutes.js
+++ src/components/routing/AppRoutes.js
@@ -10,11 +10,11 @@ const AppRoute = () => {
   return (
     <section className="container py-2" style={{ marginTop: "2vh" }}>
       <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/portfolio" element={<Portfolio />} />
-        <Route exact path="/portfolio/:id" element={<PortfolioDescription />} />
-        <Route path="/*" element={<NotFound />} />
+        <Route index path="/professional-portfolio" element={<Home />} />
+        <Route exact path="/professional-portfolio/about" element={<About />} />
+        <Route exact path="/professional-portfolio/portfolio" element={<Portfolio />} />
+        <Route exact path="/professional-portfolio/portfolio/:id" element={<PortfolioDescription />} />
+        <Route path="/professional-portfolio/*" element={<NotFound />} />
       </Routes>
     </section>
   );
